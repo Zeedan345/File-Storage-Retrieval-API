3943a93d3e18fa170b20a380d3ab75581d9bc8e5
Zeedan345 <36732475+Zeedan345@users.noreply.github.com>
2024-08-31 16:17:00 -0500

    Creating a dummy Express API

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-const app = reqiure('express');
+const express = require('express');
+const app = express();
+const PORT = 8005;
+
+app.use(express.json())
+app.listen(
+    PORT,
+    () => console.log(`Live on http://localhost:${PORT}`)
+)
+
+app.get('/tshirt', (req, res) => {
+    res.status(200).send({
+        tshirt: 'T-Shirt',
+        size: 'Large'
+    })
+});
+
+app.post('/tshirt/:id', (req, res) => {
+    const { id } = req.params;
+    const { logo } = req.body;
+
+    if (!logo) {
+        res.status(418).send({message: 'We need a logo'});
+    }
+
+    res.send({
+        tshirt: `Tshirt with the logo ${logo} and id ${id}`
+    })
+})
