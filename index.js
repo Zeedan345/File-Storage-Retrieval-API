5fb879cd640632254f1c9f076714ce125ea3ed80
Zeedan345 <36732475+Zeedan345@users.noreply.github.com>
2024-09-12 21:13:18 -0500

    Fixed some issues with update

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 const express = require('express');
-const multer = reqiure('multer');
-const archiver = reqiure('archiver');
-const fs = reqiure('fs');
-const path = reqiure('path');
+const multer = require('multer');
+const archiver = require('archiver');
+const fs = require('fs');
+const path = require('path');
 const { v4: uuidv4 } = require('uuid');
 const app = express();
 const PORT = 8005;
 
-app.use(express.json());
+//app.use(express.json());
 
 const UPLOADS_DIR = path.join(__dirname, 'uploads');
-const ZIPS_PATH = path.join(_dirname, 'zips');
+const ZIPS_PATH = path.join(__dirname, 'zips');
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
@@ -36,19 +36,19 @@ app.use('/upload', (req, res, next) => {
 });
 app.post('/upload', upload.array('files', 20),(req, res) => {
     if(!req.files || req.files.length === 0) {
-        return res.status(400).send({message: 'No file was uploaded'});
+        return res.status(400).json({message: 'No file was uploaded'});
     }
 
     const uploadId = req.uploadId;
 
-    res.status(200).send({uploadId: uploadId });
+    res.status(200).json({uploadId: uploadId });
 });
 app.get('/download/:id', async (req, res) => {
     const uploadId = req.params.id;
     const uploadPath = path.join(UPLOADS_DIR, uploadId);
 
     if (!fs.existSync(uploadPath)) {
-        return res.status(400).send({message: 'No File with that ID does not exist'});
+        return res.status(400).json({message: 'No File with that ID does not exist'});
     }
 
     const zipFileName = `file_${uploadId}.zip`;
@@ -62,7 +62,7 @@ app.get('/download/:id', async (req, res) => {
         res.download(zipFilePath, zipFileName, (err) => {
             if (err) {
                 console.error(err);
-                res.status(500).send('Error downloading the file.');
+                res.status(500).json('Error downloading the file.');
             }
 
             // Optionally, delete the zip file after download
@@ -78,7 +78,7 @@ app.get('/download/:id', async (req, res) => {
 
     archive.on('error', (err) => {
         console.error('Archiver error:', err);
-        res.status(500).send('Error creating zip file.');
+        res.status(500).json('Error creating zip file.');
     });
 
     archive.pipe(output);
