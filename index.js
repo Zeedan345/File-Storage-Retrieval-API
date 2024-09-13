583e53a9200feb085e8ee4faec53d644aeb2e544
Zeedan345 <36732475+Zeedan345@users.noreply.github.com>
2024-09-12 21:20:31 -0500

    Fixed Some issues with uploadID and download

--- index.js
+++ index.js
@@ -13,27 +13,30 @@ const UPLOADS_DIR = path.join(__dirname, 'uploads');
 const ZIPS_PATH = path.join(__dirname, 'zips');
 
 const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        const uploadId = req.uploadId;
-        const uploadPath = path.join(UPLOADS_DIR, uploadId);
-
-        if (!fs.existSync(uploadPath)) {
-            fs.mkdirSync(uploadPath);
+    destination: (req, file, cb) => {
+        // Create a directory for each upload using the uploadId
+        const uploadDir = path.join(UPLOADS_DIR, req.uploadId);
+        
+        // Check if the directory exists, if not create it
+        if (!fs.existsSync(uploadDir)) {
+            fs.mkdirSync(uploadDir);
         }
-        cb(null, uploadPath)
-    },
-    filename: function (req, file, cb) {
-        cb(null, file.orignalname);
-    }
 
-})
+        cb(null, uploadDir);
+    },
+    filename: (req, file, cb) => {
+        const uniqueSuffix = Date.now() + '-' + file.originalname;
+        cb(null, uniqueSuffix);
+    }
+});
 
 const upload = multer({storage : storage});
 
 app.use('/upload', (req, res, next) => {
-    req.uploadId = uuidv4;
+    req.uploadId = uuidv4();
     next();
 });
+
 app.post('/upload', upload.array('files', 20),(req, res) => {
     if(!req.files || req.files.length === 0) {
         return res.status(400).json({message: 'No file was uploaded'});
@@ -47,12 +50,12 @@ app.get('/download/:id', async (req, res) => {
     const uploadId = req.params.id;
     const uploadPath = path.join(UPLOADS_DIR, uploadId);
 
-    if (!fs.existSync(uploadPath)) {
-        return res.status(400).json({message: 'No File with that ID does not exist'});
+    if (!fs.existsSync(uploadPath)) {
+        return res.status(400).json({ message: 'No file with that ID exists' });
     }
 
     const zipFileName = `file_${uploadId}.zip`;
-    const zipFilePath = path.join(ZIPS_PATH, uploadID); 
+    const zipFilePath = path.join(ZIPS_PATH, zipFileName);
     const output = fs.createWriteStream(zipFilePath);
     const archive = archiver('zip', { zlib: { level: 9 } });
 
@@ -64,15 +67,6 @@ app.get('/download/:id', async (req, res) => {
                 console.error(err);
                 res.status(500).json('Error downloading the file.');
             }
-
-            // Optionally, delete the zip file after download
-            // fs.unlink(zipFilePath, (err) => {
-            //     if (err) {
-            //         console.error('Error deleting zip file:', err);
-            //     } else {
-            //         console.log(`Deleted zip file: ${zipFileName}`);
-            //     }
-            // });
         });
     });
 
@@ -86,7 +80,8 @@ app.get('/download/:id', async (req, res) => {
     archive.directory(uploadPath, false);
 
     archive.finalize();
-})
+});
+
 
 app.listen(
     PORT,
